f129e4b658da58732bf2c2536f272422f7976d30
YamJebeh <142457837+YamJebeh@users.noreply.github.com>
2023-08-19 08:05:15 +0700

    Add files via upload

---
+++ Crack.sh
@@ -0,0 +1,341 @@
+pkg install figlet
+pkg install cowsay
+#date/bin/bash
+clear #hapus layar
+clear
+q="-e"
+e="echo"
+s="sleep 1"
+ss="sleep 2"
+sss="sleep 3"
+m="\033[1;31m"
+h="\033[1;32m"
+k="\033[1;33m"
+b="\033[1;34m"
+bl="\033[1;36m"
+p="\033[1;37m"
+while [ "$else" = "$ulang" ] #input pengulangan
+do
+sleep 2
+clear
+echo $q $b
+figlet " WeLcomE"
+echo $q $bl
+echo
+echo
+read -p " PASSWORD > " apa #password
+if [ "$apa" = "minimal izin" ]
+then
+   echo $q $k
+        sleep 0
+	echo -ne " Loading [1]\r"
+        sleep 0
+        echo -ne " Loading [2]\r"
+        sleep 0
+        echo -ne " Loading [3]\r"
+        sleep 0
+        echo -ne " Loading [4]\r"
+        sleep 0
+        echo -ne " Loading [5]\r"
+        sleep 0
+        echo -ne " Loading [6]\r"
+        sleep 0
+        echo -ne " Loading [7]\r"
+        sleep 0
+        echo -ne " Loading [8]\r"
+        sleep 0
+        echo -ne " Loading [9]\r"
+        sleep 0
+        echo -ne " Loading [10]\r"
+        sleep 0
+        echo -ne " Loading [11]\r"
+        sleep 0
+        echo -ne " Loading [12]\r"
+        sleep 0
+        echo -ne " Loading [13]\r"
+        sleep 0
+        echo -ne " Loading [14]\r"
+        sleep 0
+        echo -ne " Loading [15]\r"
+        sleep 0
+        echo -ne " Loading [16]\r"
+        sleep 0
+        echo -ne " Loading [17]\r"
+        sleep 0
+        echo -ne " Loading [18]\r"
+        sleep 0
+        echo -ne " Loading [19]\r"
+        sleep 0
+        echo -ne " Loading [20]\r"
+        sleep 0
+        echo -ne " Loading [21]\r"
+        sleep 0
+        echo -ne " Loading [22]\r"
+        sleep 0
+        echo -ne " Loading [23]\r"
+        sleep 0
+        echo -ne " Loading [24]\r"
+        sleep 0
+        echo -ne " Loading [25]\r"
+        sleep 0
+        echo -ne " Loading [26]\r"
+        sleep 0
+        echo -ne " Loading [27]\r"
+        sleep 0
+        echo -ne " Loading [28]\r"
+        sleep 0
+        echo -ne " Loading [29]\r"
+        sleep 0
+        echo -ne " Loading [30]\r"
+        sleep 0
+        echo -ne " Loading [31]\r"
+        sleep 0
+        echo -ne " Loading [32]\r"
+        sleep 0
+        echo -ne " Loading [33]\r"
+        sleep 0
+        echo -ne " Loading [34]\r"
+        sleep 0
+        echo -ne " Loading [35]\r"
+        sleep 0
+        echo -ne " Loading [36]\r"
+        sleep 0
+        echo -ne " Loading [37]\r"
+        sleep 0
+        echo -ne " Loading [38]\r"
+        sleep 0
+        echo -ne " Loading [39]\r"
+        sleep 0
+        echo -ne " Loading [40]\r"
+        sleep 0
+        echo -ne " Loading [41]\r"
+        sleep 0
+        echo -ne " Loading [42]\r"
+        sleep 0
+        echo -ne " Loading [43]\r"
+        sleep 0
+        echo -ne " Loading [44]\r"
+        sleep 0
+        echo -ne " Loading [45]\r"
+        sleep 0
+        echo -ne " Loading [46]\r"
+        sleep 0
+        echo -ne " Loading [47]\r"
+        sleep 0
+        echo -ne " Loading [48]\r"
+        sleep 0
+        echo -ne " Loading [49]\r"
+        sleep 0
+        echo -ne " Loading [50]\r"
+        sleep 0
+        echo -ne " Loading [51]\r"
+        sleep 0
+        echo -ne " Loading [52]\r"
+        sleep 0
+        echo -ne " Loading [53]\r"
+        sleep 0
+        echo -ne " Loading [54]\r"
+        sleep 0
+        echo -ne " Loading [55]\r"
+        sleep 0
+        echo -ne " Loading [56]\r"
+        sleep 0
+        echo -ne " Loading [57]\r"
+        sleep 0
+        echo -ne " Loading [58]\r"
+        sleep 0
+        echo -ne " Loading [59]\r"
+        sleep 0
+        echo -ne " Loading [60]\r"
+        sleep 0
+        echo -ne " Loading [61]\r"
+        sleep 0
+        echo -ne " Loading [62]\r"
+        sleep 0
+        echo -ne " Loading [63]\r"
+        sleep 0
+        echo -ne " Loading [64]\r"
+        sleep 0
+        echo -ne " Loading [65]\r"
+        sleep 0
+        echo -ne " Loading [66]\r"
+        sleep 0
+        echo -ne " Loading [67]\r"
+        sleep 0
+        echo -ne " Loading [68]\r"
+        sleep 0
+        echo -ne " Loading [69]\r"
+        sleep 0
+        echo -ne " Loading [70]\r"
+        sleep 0
+        echo -ne " Loading [71]\r"
+        sleep 0
+        echo -ne " Loading [72]\r"
+        sleep 0
+        echo -ne " Loading [73]\r"
+        sleep 0
+        echo -ne " Loading [74]\r"
+        sleep 0
+        echo -ne " Loading [75]\r"
+        sleep 0
+        echo -ne " Loading [76]\r"
+        sleep 0
+        echo -ne " Loading [77]\r"
+        sleep 0
+        echo -ne " Loading [78]\r"
+        sleep 0
+        echo -ne " Loading [79]\r"
+        sleep 0
+        echo -ne " Loading [80]\r"
+        sleep 0
+        echo -ne " Loading [81]\r"
+        sleep 0
+        echo -ne " Loading [82]\r"
+        sleep 0
+        echo -ne " Loading [83]\r"
+        sleep 0
+        echo -ne " Loading [84]\r"
+        sleep 0
+        echo -ne " Loading [85]\r"
+        sleep 0
+        echo -ne " Loading [86]\r"
+        sleep 0
+        echo -ne " Loading [87]\r"
+        sleep 0
+        echo -ne " Loading [88]\r"
+        sleep 0
+        echo -ne " Loading [89]\r"
+        sleep 0
+        echo -ne " Loading [90]\r"
+        sleep 0
+        echo -ne " Loading [91]\r"
+        sleep 0
+        echo -ne " Loading [92]\r"
+        sleep 0
+        echo -ne " Loading [93]\r"
+        sleep 0
+        echo -ne " Loading [94]\r"
+        sleep 0
+        echo -ne " Loading [95]\r"
+        sleep 0
+        echo -ne " Loading [96]\r"
+        sleep 0
+        echo -ne " Loading [97]\r"
+        sleep 0
+        echo -ne " Loading [98]\r"
+        sleep 0
+        echo -ne " Loading [99]\r"
+        sleep 0
+        echo -ne " Loading [100]\r"
+        clear
+        echo -ne $q $h" BERHASIL LOGIN!!!\r"
+        sleep 1
+        clear
+while [ "$else" = "$ulang" ]
+do
+sleep 1
+echo $q $bl
+cowsay -f eyes MR.ONE | lolcat
+echo
+echo $q $k "[$m 01$k ]$h CRACK FACEBOOK & INSTGRAM"
+echo $q $k "[$m 02$k ]$h CRACK FACEBOOK ROBET"
+echo $q $k "[$m 03$k ]$h CRACK FACEBOOK BRAYEN3.4"
+echo $q $k "[$m EX$k ]$h KELUAR"
+echo $q $bl
+echo
+echo
+echo
+read -p " PILIH > " no
+echo
+clear
+if [ "$no" = "01" ]
+then
+   echo $q $k"Menginstall Package"
+   echo $q $h
+   apt update
+   apt upgrade
+   termux-setup-storage
+   pkg install python
+   pkg install git
+   pip install requests
+   pip install bs4
+   pip install pyotp
+   pip install futures
+   pip install pycryptodome
+   pip install stdiomask
+   pip install struct
+   git clone https://github.com/Dicky-XD/MSF2
+   cd MSF2
+   echo $q $k
+   echo "Selesai Penginstalan Package"
+   git pull
+   python run.py
+elif [ "$no" = "02" ]
+then
+   echo $q $k"MENGINSTALL SCRIP"
+   echo $q $h
+   pkg update
+   pkg upgrade
+   apt update
+   apt upgrade
+   pkg install python-pip
+   pip install requests
+   pip install bs4
+   pip install bash
+   cd $HOME
+   cd
+   git clone https://github.com/RobetX/crack.fb
+   cd crack.fb
+   pip install rich
+   pip install stdiomask
+   echo $q $k
+   echo "SELESAI MENGINSTALL SCRIP"
+   chmod +x *
+   git pull
+   python 'sc v1.py'
+elif [ "$no" = "03" ]
+then
+   echo $q $k"Menginstall Package"
+   echo $q $h
+   pkg update
+   pkg upgrade
+   apt update
+   apt upgrade
+   pkg install git
+   pkg install python
+   pip install stdiomask
+   pip install requests
+   pip install bs4
+   pip install requests
+   pip install rich
+   pip install mechanize
+   cd $HOME
+   cd
+   git clone https://github.com/BrayennnXD/Brayennn3.4
+   cd Brayennn3.4
+   echo $q $k
+   echo "Selesai Penginstalan Package"
+   chmod +x *
+   git pull
+   python BrayennnFB.py
+elif [ "$no" = "EX" ]
+then
+echo $q $bl
+clear
+figlet "BerhasiL Out"
+exit
+else
+	clear
+	sleep 1
+	echo " KESALAHAN INPUT"
+	sleep 1
+	clear
+fi
+done
+else
+clear
+sleep 2
+	echo -ne $q $m"  password salah\r"
+sleep 2
+fi
+done
